6784919e42fbef0b2e0274e200e3c3b7cf6600b1
VasiaPupkin357 <ivanzadov2000@gmail.com>
2020-05-07 13:07:35 +0300

    111

--- tests/com/chess/tests/TestBoard.java
+++ tests/com/chess/tests/TestBoard.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestBoard {
-//
+
     @Test
     public void testBoardConsistency() {
         final Board board = Board.createStandardBoard();
